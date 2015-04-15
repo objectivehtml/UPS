d8122129499421d48e2a983013ead5c98192d05a
Tamara Fedorova <koul.slou@gmail.com>
2015-04-14 22:47:00 -0500

    Fix address array generation in build_location function

--- libraries/Ups/Ups_Live_Rates.php
+++ libraries/Ups/Ups_Live_Rates.php
@@ -244,13 +244,13 @@ class Ups_Live_Rates extends Base_Ups
     	parent::__construct($data);
     }
         
-    protected function build_location($var)
+    protected function build_location($argLocation)
     {
     	$components = array(
     		'company',
     		'attn',
-    		'phone', 
-    		'fax', 
+    		'phone',
+    		'fax',
     		'address' => array(
     			'street',
     			'street_2',
@@ -261,58 +261,48 @@ class Ups_Live_Rates extends Base_Ups
     			'country_code'
     		)
     	);
-    	
-	    if(!is_array($var) && !is_object($var) && preg_match('/\d{5}/', $var))
-	    {
-		    $var = array(
-		    	'address' => array(
-		    		'state'        => $this->get_state($var),
-		    		'postal_code'  => $var,
-		    		'country_code' => $this->country_code
-		    	)
-		    );
-	    }
-	    
-	    foreach($components as $index => $component)
-	    {		
-	    	$component_name = $component;
-	    	
-	    	$is_array = TRUE;
-	    	
-	    	if(!is_array($component))
-	    	{
-	    		$is_array  = FALSE;
-		    	$component = array($component);	
-	    	}
-	    	else
-	    	{
-		    	$component_name = $index;
-	    	}
-	    	
-	    	foreach($component as $sub_component)
-	    	{
-	    		$value = NULL;
-	    		
-				if(!isset($var[$sub_component]) && property_exists($this, $sub_component))
-				{
-					$value = $this->$sub_component;
-				}
-				
-				if(!empty($value))
-				{
-					if($is_array)
-					{
-						$var[$component_name][$sub_component] = $value;
-					}
-					else
-					{
-				    	$var[$sub_component] = $value;
-				    }
-			    }
-		    }   
-	    }
-	      
-	    return $var;
+
+
+        //If just zip code string is passed
+        if(is_string($argLocation))
+        {
+            if(!preg_match('/^\d{5}$/', $argLocation)){
+                throw new Exception("Bad format for zip code in ".__FUNCTION__."() function in class ".__CLASS__." on line ".__LINE__);
+            }
+
+            return array('address' => array(
+                'state' => $this->get_state($argLocation),
+                'postal_code' => $argLocation,
+                'country_code' => $this->country_code
+            ));
+
+        }
+
+
+        $location = array();
+
+        //If address passed in array format
+        foreach($components as $index=>$addrComponentName){
+
+            //Work with strings - company, attn, phone, fax
+            if(is_string($components[$index])){
+                if(isset($argLocation[$addrComponentName]) && !empty($argLocation[$addrComponentName])){
+                    $location[$addrComponentName] = $argLocation[$addrComponentName];
+                }
+            //Work with address inner components - street, city, state, postal_code, country_code
+            } elseif(is_array($components[$index])){
+
+                $outerElementName = $index;
+                foreach($components[$outerElementName] as $innerElement){
+                    if(isset($argLocation[$innerElement]) && !empty($argLocation[$innerElement])){
+                        $location[$outerElementName][$innerElement] = $argLocation[$innerElement];
+                    }
+                }
+
+            }
+
+        }
+        return $location;
     }
     
     protected function key($str)
@@ -511,7 +501,7 @@ class Ups_Live_Rates extends Base_Ups
                 </RateInformation>
             </Shipment>
         </RatingServiceSelectionRequest>";
-        
+
         $result = $this->curl($data);
         
         $xml = new SimpleXMLElement(strstr($result, '<?'));
